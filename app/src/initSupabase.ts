11111f7a43c78fc5b8db5b75a014c8b7c1e08f61
renan <renanlemos21@gmail.com>
2023-07-16 20:33:50 -0300

    feat: add lib to handle .env files

--- app/src/initSupabase.ts
+++ app/src/initSupabase.ts
@@ -1,8 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import {API_URL, API_KEY} from "@env"
 
 // Better put your these secret keys in .env file
-export const supabase = createClient("supabaseUrl", "supabaseKey", {
+export const supabase = createClient(API_URL, API_KEY, {
   localStorage: AsyncStorage as any,
   detectSessionInUrl: false // Prevents Supabase from evaluating window.location.href, breaking mobile
 });
