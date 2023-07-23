ff6489c15ea7c89720a75bcbdab4db5d3e329865
renan <renanlemos21@gmail.com>
2023-07-23 10:37:54 -0300

    refactor: remove unnecessary lib to .env after react native update

--- app/src/initSupabase.ts
+++ app/src/initSupabase.ts
@@ -1,10 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
-import { API_URL, API_KEY } from '@env'
 
 // Better put your these secret keys in .env file
-export const supabase = createClient(API_URL, API_KEY, {
+export const supabase = createClient(process.env.API_URL as string, process.env.API_KEY as string, {
   localStorage: AsyncStorage as any,
   detectSessionInUrl: false // Prevents Supabase from evaluating window.location.href, breaking mobile
 })
