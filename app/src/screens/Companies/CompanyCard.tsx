62cf0f77827669c62c69a78fc9deed4770c65902
renan <renanlemos21@gmail.com>
2023-07-24 00:02:40 -0300

    feat: add shadow to the company card

--- app/src/screens/Companies/CompanyCard.tsx
+++ app/src/screens/Companies/CompanyCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text } from 'react-native-rapi-ui'
 import { View } from 'react-native'
 import { StyleSheet } from 'react-native'
+import { generateBoxShadowStyle } from '../../utils/styles'
 
 interface Location {
   latitude: number,
@@ -41,20 +42,22 @@ export default function CompanyCard(props: CompanyCardProps) {
       borderStyle: 'solid',
       borderWidth: 1,
       borderRadius: 10,
+      borderColor: '#c0c0c0',
       width: 300,
       height: 100,
       backgroundColor: '#D9D9D9'
-    }
+    },
+    shadow: generateBoxShadowStyle(-2, 4, 0.2, 3, 3)
   })
   return (
     <View
-      style={styles.container}
+      style={[styles.container, styles.shadow]}
     >
       {
         address && (
           <View style={[{ display: 'flex', height: '100%' }]}>
             <View style={{ display: 'flex', flexDirection: 'row', flexWrap:'nowrap' }}>
-              <Text fontWeight="bold" size="xl">{props.name}</Text>
+              <Text fontWeight="bold" size="xl" style={{ textTransform: 'capitalize' }}>{props.name}</Text>
               {/*TODO: Add this values dynamically in the future*/}
               <Text style={{ marginLeft: 'auto' }}>800m</Text>
             </View>
