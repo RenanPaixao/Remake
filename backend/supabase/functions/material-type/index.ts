1586cbb76e774c26ff9e53cba344460f071c092b
Alberto José <49288213+albertojcvs@users.noreply.github.com>
2023-07-25 17:52:45 -0300

    feat: add edge function to use gpt

---
+++ backend/supabase/functions/material-type/index.ts
@@ -0,0 +1,48 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
+console.log("Hello from Functions!");
+
+serve(async (req) => {
+  const a = Deno.env.get("GPT_KEY");
+
+  console.log("aa", a);
+  console.log("bb", req);
+  const body = await req.json();
+  if (!body?.product) {
+    return new Response(
+      JSON.stringify({
+        error: "Não foi enviado um produto!",
+        message: "Não foi enviado um produto!",
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 400,
+      }
+    );
+  }
+  const gptResponse = await axiod.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Um caderno é descartado no lixo reciclável pois é predominantemente composto por papel, levando em consideração a predominância dos materiais e as seguintes classificações: Lixo-Orgânico Lixo Reciclável Lixo Eletrônico Resíduos Perigosos Lixo não Reciclável Em qual categoria podemos classificar o seguinte produto: ${body.product}? Retorne apenas a categoria no formato JSON contendo como atributos o nome, a categoria e uma justificativa para encaixá-lo na categoria retornada.`,
+        },
+      ],
+      temperature: 0,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${a}`,
+      },
+    }
+  );
+  console.log("ja foi gpt");
+  console.log(gptResponse);
+
+  const responseData = JSON.parse(gptResponse.data.choices[0].message.content);
+  return new Response(JSON.stringify(responseData), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
