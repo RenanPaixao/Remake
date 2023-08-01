8f405c0298f692aaf748bbe02d53589610c97551
renan <renanlemos21@gmail.com>
2023-07-31 23:50:16 -0300

    refactor: update schemaNormalize to identify and return types correctly

--- app/src/utils/schemaNormalize.ts
+++ app/src/utils/schemaNormalize.ts
@@ -2,8 +2,11 @@
  * Transform an object in camel case to an object in snake case to be used in with the supabase API.
  * @param schema The object to be transformed.
  */
-export const schemaNormalize = <T>(schema: T) => {
-  const normalizedSchema = {} as Record<string, any>
+export const schemaNormalize = <T extends Record<string, any>>(schema: T): T => {
+  type Types = {
+    [k in keyof T]: any
+  }
+  const normalizedSchema = {} as Types
 
   for (const key in schema) {
     const newKey = key.replace(/([A-Z])/g, '_$1').toLowerCase()
