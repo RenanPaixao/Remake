7f309ad5642e32d3847abb167b562e22b865cd89
renan <renanlemos21@gmail.com>
2023-08-22 21:12:24 -0300

    feat: add modal with details

--- app/src/screens/Companies/Companies.tsx
+++ app/src/screens/Companies/Companies.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from 'react'
-import { FlatList, TouchableHighlight, View } from 'react-native'
+import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react'
+import { FlatList, StyleSheet, TouchableHighlight, View } from 'react-native'
 import { MainStackParamList } from '../../types/navigation'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { Button, Layout, Text, TopNav } from 'react-native-rapi-ui'
@@ -9,12 +9,15 @@ import { useQuery } from '@tanstack/react-query'
 import Loading from '../utils/Loading'
 import { LocationContext } from '../../provider/LocationProvider'
 import haversine from 'haversine-distance'
+import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function Companies({
   navigation
 }: NativeStackScreenProps<MainStackParamList, 'Companies'>) {
 
   const { location, updateLocation } = useContext(LocationContext)
+  const [selectedCompany, setSelectedCompany] = useState(null)
 
   const { isLoading, data } = useQuery({
     queryKey: ['companies', location],
@@ -41,12 +44,22 @@ export default function Companies({
     })()
   }, [updateLocation])
 
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null)
+  const snapPoints = useMemo(() => ['25%'], [])
+  const presentModal = useCallback(() => {
+    bottomSheetModalRef.current?.present()
+  }, [])
+
+  const closeModal = useCallback(() => {
+    bottomSheetModalRef.current?.dismiss()
+  }, [])
+
   return (
     <Layout>
       <TopNav
         middleContent="Pontos de coleta"
       />
-      <View
+      <GestureHandlerRootView
         style={{
           flex: 1,
           alignItems: 'center',
@@ -87,11 +100,15 @@ export default function Companies({
                     }) : 0
 
                     return <TouchableHighlight
-                      onPress={() => navigation.navigate('LocationDetails', company.locations[0])}
+                      onPress={() => {
+                        setSelectedCompany(company)
+                        presentModal()
+                      }}
                       style={{ paddingVertical: 10, marginHorizontal: 5 }}
                       underlayColor="transparent"
                     >
-                      <CompanyCard id={company.id}
+                      <CompanyCard
+                        id={company.id}
                         name={company.name}
                         location={nearerCompanyLocation}
                         distance={distance}
@@ -102,7 +119,46 @@ export default function Companies({
               </>
           }
         </View>
-      </View>
+        <BottomSheetModalProvider>
+          <View style={styles.container}>
+            <BottomSheetModal
+              ref={bottomSheetModalRef}
+              snapPoints={snapPoints}
+            >
+              <View style={styles.contentContainer}>
+                <Text style={{ textAlign: 'center', fontWeight: 'bold', marginTop: 10, marginBottom: 30 }}>
+                  {selectedCompany?.name}
+                </Text>
+                <View style={styles.buttonContainer}>
+                  <Button text={'Fechar'} onPress={() => closeModal()} outline />
+                  <Button
+                    text={'Ver detalhes...'}
+                    onPress={() => navigation.navigate('LocationDetails', selectedCompany?.locations[0])}
+                  />
+                </View>
+              </View>
+            </BottomSheetModal>
+          </View>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </Layout >
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: '20%',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    position: 'absolute'
+  },
+  contentContainer: {
+    flex: 1
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  }
+})
